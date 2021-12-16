ecc4c127b338803b7dffcccf64a321f1d7292e4a
AlyonaBabik <pirogova0999@mail.ru>
2021-12-16 15:20:19 +0700

    Practice

---
+++ practice_16/testCats.py
@@ -0,0 +1,6 @@
+from cat import Cat
+
+cat_1 = Cat('Барон', 'мальчик', '2 года')
+cat_2 = Cat('Сэм', "мальчик", '2 года')
+print('Первый кот - ', cat_1.name, cat_1.gender, cat_1.age)
+print('Второй кот - ', cat_2.name, cat_2.gender, cat_2.age)
